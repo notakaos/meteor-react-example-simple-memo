b3c949191806bbfdc4027e50ae316c70f4c77074
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-05-14 20:47:32 +0900

    change title

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <h1>Simple Memo on Heroku</h1>
+        <h1>Review apps test</h1>
       </header>
     );
   }
